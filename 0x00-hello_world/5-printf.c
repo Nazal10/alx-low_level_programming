e4edf294c4cb98e1f53fd66413fb0f40983f9e97
nazal10 <nazal10line@gmail.com>
2023-06-15 11:53:45 -0700

    lets finish it

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,10 @@
  * a piece of art," followed by a new line. 
  * Return: always 0
  */
+
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n";
-        return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return 0;
 }
+
