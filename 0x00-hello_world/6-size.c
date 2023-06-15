0e4cb5a06a9d9aefbde1a223bf4645afc894f437
nazal10 <nazal10line@gmail.com>
2023-06-15 12:30:00 -0700

    nazall

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - Prints the size of various typesa based on
- * the computer it is compiled and run on ..
- * Return: Always 0.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
         printf("Size of a char: %zu byte(s)\n", sizeof(char));
