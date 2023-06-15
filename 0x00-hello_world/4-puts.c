a12ced73d7c46bc3914eae36d8cfdda775fe03be
nazal10 <nazal10line@gmail.com>
2023-06-15 07:35:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include <stdio.h
+
+
 /**
- * * main - prints "Programming is like building a multilingual puzzle,
- * followed by a newline.
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual
+ * puzzle. followed by a newline.
+ * Return: Always 0.
  */
 int main(void)
+
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
