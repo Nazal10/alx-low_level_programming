fef1ebd512379b78c3d5a13833185e8b25454fde
nazal10 <nazal10line@gmail.com>
2023-06-15 10:57:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle. followed by a newline.
- * Return: Always 0.
+ * puzzle, followed by a newline
+ * Return: Always 0
  */
+
 int main(void)
 
 {
