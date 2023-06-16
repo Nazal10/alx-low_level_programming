1f1662e83d8ed298f9d1da5c87a210012ff9ca61
nazal10 <nazal10line@gmail.com>
2023-06-16 14:41:25 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,15 +8,15 @@
 */
 
 int main(void)
-{       
-        int i;
+{
+	int i;
 
-        for (i = 0; i < 10; i++)
-        {
-		printf("%d", i);
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
 	}
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
 
