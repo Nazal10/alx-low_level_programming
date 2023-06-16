5ea92e25f98046a16f795447309bd23d9febc517
nazal10 <nazal10line@gmail.com>
2023-06-16 14:16:20 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,14 @@
 
 int main(void)
 {
- 	
-	char alp[52]= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-		puthcar(alp[i]);
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
 	}
-	putcher('\n')
-        return (0);
+	putchar('\n');
+	return (0);
 }
 
