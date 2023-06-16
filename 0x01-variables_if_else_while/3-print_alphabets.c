0b8928eaf87e357207996c5ce4c1c96d1657ed71
nazal10 <nazal10line@gmail.com>
2023-06-16 12:28:29 -0700

    mca

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+ 	
+	char alp[52]= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+		puthcar(alp[i]);
+	}
+	putcher('\n')
+        return (0);
+}
