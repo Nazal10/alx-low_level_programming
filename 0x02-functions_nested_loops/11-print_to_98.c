7d8f4a6a8e9289e6f13f48c5b201930dee5767e9
nazal10 <nazal10line@gmail.com>
2023-06-20 16:32:45 -0700

    adafdd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,19 @@
  */
 void print_to_98(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i < 98; i++)
-            printf("%d, ", i);
-    }
-    else
-    {
-        for (i = n; i > 98; i--)
-            printf("%d, ", i);
-    }
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	}
 
-    printf("98\n");
+	printf("98\n");
 }
 
