ae36543b0408d8f4ca53690cec78ad79b6a18b77
nazal10 <nazal10line@gmail.com>
2023-06-20 13:48:32 -0700

    2-main.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet ten times
+ */
+void print_alphabet_x10(void)
+{
+    int i;
+    char letter;
+
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
+
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
+
+        _putchar('\n');
+    }
+}
+
