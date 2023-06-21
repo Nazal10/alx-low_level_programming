fe3287c8559d7069cd81ad187e6b1f6a72cc40a5
nazal10 <nazal10line@gmail.com>
2023-06-21 02:09:20 -0700

    adaf

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,20 +1,36 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number for which the times table should be printed.
  */
-int main(void)
+void print_times_table(int n)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);
+  int i, j, result;
 
-    return (0);
+  if (n > 15 || n < 0)
+    return;
+
+  for (i = 0; i <= n; i++)
+  {
+    for (j = 0; j <= n; j++)
+    {
+      result = i * j;
+      if (j != 0)
+      {
+        _putchar(',');
+        _putchar(' ');
+
+        if (result < 10)
+          _putchar(' ');
+        if (result < 100)
+          _putchar(' ');
+      }
+      _putchar(result / 100 + '0');
+      _putchar((result / 10) % 10 + '0');
+      _putchar(result % 10 + '0');
+    }
+    _putchar('\n');
+  }
 }
 
